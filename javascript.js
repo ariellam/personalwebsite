c095b4c216e76907e027f92f5bc0d4ad722298d7
Ariel Lam <ariel.lam8@gmail.com>
2018-01-29 00:01:29 -0500

    Added artwork, fixed up about, etc

--- javascript.js
+++ javascript.js
@@ -7,6 +7,28 @@ $(window).on('load', function() { // makes sure the whole site is loaded
 })
 
 $(document).ready(function() {
+  $('.smooth').on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+
 	// for the text caption carousel
   	$('#filterText').html($('.active > .carousel-caption').html());
 
