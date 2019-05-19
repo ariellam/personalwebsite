fd689ef3f45d6f15c2bfe86235ddfb83afbad6cc
Ariel Lam <ariel.lam8@gmail.com>
2019-05-18 22:47:53 -0400

    testing new styles, added react component to test

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+var header = document.getElementById("top-menu");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
 
 var i = 1;
 $(window).on('load', function() { // makes sure the whole site is loaded 
