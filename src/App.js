2c9e05b3cae9b3b303a19c6bfcaeac0435a35054
Ariel Lam <ariel.lam8@gmail.com>
2020-02-20 08:57:51 -0500

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Projects from './components/projects/projects'
 import Gallery from './components/gallery/gallery'
 import Experience from './components/experience/experience'
 import Footer from './components/footer'
+import ReactGA from 'react-ga';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-146348600-1');
+  ReactGA.pageview('/homepage');
+}
 
 function App() {
   return (
